7b2fa0ca257cf496720c7beb47fcfa16cc757a4d
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-02-03 16:14:09 +0800

    drivebase

---
+++ src/main/java/frc/robot/component/drivebase.java
@@ -0,0 +1,33 @@
+package frc.robot.component;
+
+import org.team6083.lib.drive.DifferentialDrive;
+import org.team6083.lib.drive.inputs.TankInput;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
+import frc.robot.Robot;
+
+public class drivebase {
+    public static DifferentialDrive drive;
+    public static VictorSP rightFront;
+    public static VictorSP rightBack;
+    public static VictorSP leftFront;
+    public static VictorSP leftBack;
+    private static final int LF = 0;
+    private static final int LB = 1;
+    private static final int RF = 2;
+    private static final int RB = 3;
+
+    public static void init() {
+        rightFront = new VictorSP(RF);
+        rightBack = new VictorSP(RB);
+        leftBack = new VictorSP(LB);
+        leftFront = new VictorSP(LF);
+        drive = new DifferentialDrive(rightBack, rightFront, leftBack, leftFront);
+    }
+
+    public static void teleOp() {
+        drive.tankDrive(Robot.xbox);
+    }
+}
