2bec27bb04db99194bfb74ae45de6388cd61b422
Anna0405 <anna20050405@gmail.com>
2021-02-05 15:53:51 +0800

    [add]

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+  Joystick.joy;
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
@@ -44,7 +45,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-
+    suck.teleop();
   }
 
   @Override
