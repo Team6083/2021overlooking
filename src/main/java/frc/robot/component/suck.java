c976c33bd3c1631a075d67e3803ecd5fae42695d
Anna0405 <anna20050405@gmail.com>
2021-04-13 20:45:59 +0800

    i dunno if it can suck right

--- src/main/java/frc/robot/component/suck.java
+++ src/main/java/frc/robot/component/suck.java
@@ -6,15 +6,23 @@ import frc.robot.Robot;
 
 public class suck{
     private static WPI_VictorSPX suck;
+    private static Compressor com1;
+    private static Compressor com2;
 
     public static void init(){
          suck =  new WPI_VictorSPX( 1 );
+         com1 = new Compressor();
+         com2 = new Compressor()
     }
 
     public static void teleop(){
         if(Robot.xbox.getXButton()) {
+            com1.setClosedLoopControl(true);
+            com2.setClosedLoopControl(true);
             suck.set(ControlMode.PercentOutput, 0.5);
         }else{
+            com1.setClosedLoopControl(false);
+            com2.setClosedLoopControl(false);
             suck.set(ControlMode.PercentOutput, 0);
         }
         
