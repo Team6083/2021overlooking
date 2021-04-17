8bcd86dd1f86a342aa1ae95a9a9c00d6182c6805
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-04-17 16:00:15 +0800

    new sucking

--- src/main/java/frc/robot/component/suck.java
+++ src/main/java/frc/robot/component/suck.java
@@ -2,30 +2,56 @@ package frc.robot.component;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.robot.Robot;
 
 public class suck{
     private static WPI_VictorSPX suck;
-    private static Compressor com1;
-    private static Compressor com2;
+    private static Compressor comRight;
+    private static Compressor comLeft;
+    private static Boolean compreBoolean;
+    private static DoubleSolenoid SolRight;
+    private static DoubleSolenoid SolLeft;
+    private static Boolean suckBoolean; 
 
     public static void init(){
-         suck =  new WPI_VictorSPX( 1 );
-         com1 = new Compressor();
-         com2 = new Compressor()
+         suck =  new WPI_VictorSPX(1);
+         comRight = new Compressor();
+         comLeft = new Compressor();
+         SolRight = new DoubleSolenoid(0,1);
+         SolLeft = new DoubleSolenoid(0,2);
+         compreBoolean = true;
+         suckBoolean = false;
+         comRight.setClosedLoopControl(compreBoolean);
+         comLeft.setClosedLoopControl(compreBoolean);
     }
 
     public static void teleop(){
-        if(Robot.xbox.getXButton()) {
-            com1.setClosedLoopControl(true);
-            com2.setClosedLoopControl(true);
-            suck.set(ControlMode.PercentOutput, 0.5);
-        }else{
-            com1.setClosedLoopControl(false);
-            com2.setClosedLoopControl(false);
-            suck.set(ControlMode.PercentOutput, 0);
+        if(Robot.xbox.getRawButton(5)) {
+            SolRight.set(Value.kForward);
+            SolLeft.set(Value.kForward);
         }
-        
+        else if(Robot.xbox.getRawButton(6)){
+            SolRight.set(Value.kReverse);
+            SolLeft.set(Value.kReverse);
+        }
+        else{
+            SolRight.set(Value.kOff);
+            SolLeft.set(Value.kOff);
+        }
+
+        if(Robot.xbox.getRawButton(7)){
+            suckBoolean = !suckBoolean;
+        }
+        if(suckBoolean){
+            suck.set(ControlMode.PercentOutput,0.5);
+        }
+        else{
+            suck.set(ControlMode.PercentOutput,0);
+        }
+
     }
 
 }
