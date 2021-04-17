3be6ad34ce43a4f4669c75456c2b8c88a44e8fa3
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-04-17 08:07:02 +0800

    for visionTracking and drivebase

--- src/main/java/frc/robot/component/drivebase.java
+++ src/main/java/frc/robot/component/drivebase.java
@@ -3,6 +3,8 @@ package frc.robot.component;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import org.team6083.lib.drive.DifferentialDrive;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
 public class drivebase {
@@ -22,9 +24,39 @@ public class drivebase {
         leftBack = new WPI_VictorSPX(LB);
         leftFront = new WPI_VictorSPX(LF);
         drive = new DifferentialDrive(rightBack, rightFront, leftBack, leftFront);
+     
     }
 
     public static void teleOp() {
+        if (Robot.xbox.getPOV()==0){
+            leftFront.set(0.3);
+            leftBack.set(0.3);
+            rightFront.set(-0.3);
+            rightBack.set(-0.3);
+        }
+        else if (Robot.xbox.getPOV()==180){
+            leftFront.set(-0.3);
+            leftFront.set(-0.3);
+            rightFront.set(0.3);
+            rightBack.set(0.3);
+        }
+        else if (Robot.xbox.getPOV()==90){
+            leftFront.set(0.3);
+            leftBack.set(0.3);
+            rightFront.set(0);
+            rightBack.set(0);
+        }
+        else if (Robot.xbox.getPOV()==270){
+            leftFront.set(0);
+            leftBack.set(0);
+            rightFront.set(-0.3);
+            rightBack.set(-0.3);
+        }
+        else{
         drive.tankDrive(Robot.xbox);
+        }
+    }
+    public static void track(double speed, double rotation, boolean input) {
+        drive.arcadeDrive(speed, rotation, input);
     }
 }
