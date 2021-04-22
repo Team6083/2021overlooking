b92afea7521d456d82c0eae5704c7efdbd0dd835
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-04-22 19:07:41 +0800

    base name change

--- src/main/java/frc/robot/component/drivebase.java
+++ src/main/java/frc/robot/component/drivebase.java
@@ -6,48 +6,48 @@ import frc.robot.Robot;
 
 public class drivebase {
     public static DifferentialDrive drive;
-    public static WPI_VictorSPX rightFront;
-    public static WPI_VictorSPX rightBack;
-    public static WPI_VictorSPX leftFront;
-    public static WPI_VictorSPX leftBack;
-    private static final int LF = 0;
-    private static final int LB = 1;
-    private static final int RF = 2;
-    private static final int RB = 3;
+    public static WPI_VictorSPX right_1;
+    public static WPI_VictorSPX right_2;
+    public static WPI_VictorSPX left_1;
+    public static WPI_VictorSPX left_2;
+    private static final int Left_1 = 0;
+    private static final int Left_2= 1;
+    private static final int Right_1 = 2;
+    private static final int Right_2 = 3;
 
     public static void init() {
-        rightFront = new WPI_VictorSPX(RF);
-        rightBack = new WPI_VictorSPX(RB);
-        leftBack = new WPI_VictorSPX(LB);
-        leftFront = new WPI_VictorSPX(LF);
-        drive = new DifferentialDrive(rightBack, rightFront, leftBack, leftFront);
+        right_1 = new WPI_VictorSPX(Right_1);
+        right_2 = new WPI_VictorSPX(Right_2);
+        left_2 = new WPI_VictorSPX(Left_2);
+        left_1 = new WPI_VictorSPX(Left_1);
+        drive = new DifferentialDrive(right_1, right_2, left_1, left_2);
      
     }
 
     public static void teleOp() {
         if (Robot.xbox.getPOV()==0){
-            leftFront.set(0.3);
-            leftBack.set(0.3);
-            rightFront.set(-0.3);
-            rightBack.set(-0.3);
+            left_1.set(0.3);
+            left_2.set(0.3);
+            right_1.set(-0.3);
+            right_2.set(-0.3);
         }
         else if (Robot.xbox.getPOV()==180){
-            leftFront.set(-0.3);
-            leftFront.set(-0.3);
-            rightFront.set(0.3);
-            rightBack.set(0.3);
+            left_1.set(-0.3);
+            left_2.set(-0.3);
+            right_1.set(0.3);
+            right_2.set(0.3);
         }
         else if (Robot.xbox.getPOV()==90){
-            leftFront.set(0.3);
-            leftBack.set(0.3);
-            rightFront.set(0);
-            rightBack.set(0);
+            left_1.set(0.3);
+            left_2.set(0.3);
+            right_1.set(0);
+            right_2.set(0);
         }
         else if (Robot.xbox.getPOV()==270){
-            leftFront.set(0);
-            leftBack.set(0);
-            rightFront.set(-0.3);
-            rightBack.set(-0.3);
+            left_1.set(0);
+            left_2.set(0);
+            right_1.set(-0.3);
+            right_2.set(-0.3);
         }
         else{
         drive.tankDrive(Robot.xbox);
