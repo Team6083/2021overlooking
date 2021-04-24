0325857920526e42b52c596fe697f73936736cb8
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-04-24 13:36:07 +0800

    運球要求好多...

--- src/main/java/frc/robot/component/transport.java
+++ src/main/java/frc/robot/component/transport.java
@@ -3,24 +3,29 @@ package frc.robot.component;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.Robot;
 
 public class transport {
     private static WPI_VictorSPX horizontal;
     private static WPI_VictorSPX straight;
-    private static Boolean transportBoolean;
 
     public static void init(){
-        horizontal = new WPI_VictorSPX(6);
-        straight = new WPI_VictorSPX(7);
-        transportBoolean = false;
+        horizontal = new WPI_VictorSPX(4);
+        straight = new WPI_VictorSPX(8);
     }
 
     public static void teleOp(){
-        if(Robot.xbox.getRawButton(9)){
-            transportBoolean = !transportBoolean;
+        if(Robot.xbox_2.getBumper(Hand.kLeft)){
+            horizontal.set(ControlMode.PercentOutput,-0.5);
+            straight.set(ControlMode.PercentOutput,-0.5);
         }
-        if(transportBoolean){
+        else{
+            horizontal.set(ControlMode.PercentOutput,0);
+            straight.set(ControlMode.PercentOutput,0);
+        }
+        
+        if(Robot.xbox_2.getBumper(Hand.kRight)){
             horizontal.set(ControlMode.PercentOutput,0.5);
             straight.set(ControlMode.PercentOutput,0.5);
         }
@@ -28,6 +33,21 @@ public class transport {
             horizontal.set(ControlMode.PercentOutput,0);
             straight.set(ControlMode.PercentOutput,0);
         }
+
+        if(Robot.xbox_2.getBButton()){
+            straight.set(ControlMode.PercentOutput,-0.5);
+        }
+        else{
+            straight.set(ControlMode.PercentOutput,0);
+        }
+
+        if(Robot.xbox_2.getAButton()){
+            horizontal.set(ControlMode.PercentOutput,-0.5);
+        }
+        else{
+            horizontal.set(ControlMode.PercentOutput,0);
+        }
+
     }
     public static void autonomousTransport(){
         horizontal.set(ControlMode.PercentOutput,0.5);
