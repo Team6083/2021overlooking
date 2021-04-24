ffba2ad59fd6f1f6ff877ec0b284146a1a210a9e
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2021-04-24 13:26:22 +0800

    up

--- src/main/java/frc/robot/component/up.java
+++ src/main/java/frc/robot/component/up.java
@@ -2,22 +2,37 @@ package frc.robot.component;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.Robot;
 public class up{
    
-    private static WPI_VictorSPX up;
+    private static WPI_VictorSPX hook;
+    private static WPI_VictorSPX rope;
+   
     public static void init(){
-    up =  new WPI_VictorSPX( 87 );
+    hook =  new WPI_VictorSPX(10);
+    rope = new WPI_VictorSPX(2);
     }
     public static void teleop(){
-        if(Robot.xbox.getBButton()){
-            up.set(ControlMode.PercentOutput,0.5);
+        if(Robot.xbox_1.getXButton()){
+            rope.set(ControlMode.PercentOutput,0.5);
         }
-        else if(Robot.xbox.getAButton()){
-            up.set(ControlMode.PercentOutput,-0.5);
+        else if(Robot.xbox_1.getAButton()){
+            rope.set(ControlMode.PercentOutput,0.5);
         }
         else{
-            up.set(ControlMode.PercentOutput,0);
+            rope.set(ControlMode.PercentOutput,0);
+        }
+        
+        if(Robot.xbox_2.getTriggerAxis(Hand.kRight)>0.7){
+            hook.set(ControlMode.PercentOutput,-0.5);
+        }
+        else if(Robot.xbox_2.getTriggerAxis(Hand.kLeft)>0.7){
+            hook.set(ControlMode.PercentOutput,0.5);
+        }
+        else{
+            hook.set(ControlMode.PercentOutput,0);
         }
     }
 }
