6f1f1154b6d3400d69e2f1325b9c0eebc4513c28
dora1220 <dora63ju@gmail.com>
2021-04-25 10:15:09 +0800

    add path and amp control transport

--- src/main/java/frc/robot/component/transport.java
+++ src/main/java/frc/robot/component/transport.java
@@ -3,19 +3,32 @@ package frc.robot.component;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import org.team6083.lib.RobotPower;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
 public class transport {
     private static WPI_VictorSPX top;
     private static WPI_VictorSPX buttom;
+    private static RobotPower power;
 
     public static void init(){
+        RobotPower.init(12);
         top = new WPI_VictorSPX(4);
         buttom = new WPI_VictorSPX(8);
+        power = new RobotPower(0);
+
+        
+        
     }
 
     public static void teleOp(){
-        if(Robot.xbox_2.getRawButton(5)){
+        
+        if(power.getPortCurrent()>9){
+            top.set(ControlMode.PercentOutput,-0.4);
+            buttom.set(ControlMode.PercentOutput,-0.4);
+        }else if(Robot.xbox_2.getRawButton(5)){
             top.set(ControlMode.PercentOutput,0.5);
             buttom.set(ControlMode.PercentOutput,0.5);
         }
@@ -34,9 +47,13 @@ public class transport {
             buttom.set(ControlMode.PercentOutput,0);
         }
 
+        
+
+        SmartDashboard.putNumber("Amp", power.getPortCurrent());
     }
     public static void autonomousTransport(){
         top.set(ControlMode.PercentOutput,-0.5);
         buttom.set(ControlMode.PercentOutput,-0.5);
     }
+
 }
